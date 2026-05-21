82ed0497135ea314be769627fb3cef5714505cbf
SantiGVadone <santiagovadone36@gmail.com>
2026-05-21 20:53:19 -0300

    feat: complete all sections, need more details

--- scripts.js
+++ scripts.js
@@ -1,1 +1,13 @@
-console.log('que onda')
+const hamburger = document.getElementById('hamburger')
+const navLinks = document.getElementById('nav-links')
+
+hamburger.addEventListener('click', () => {
+  navLinks.classList.toggle('active')
+})
+
+// Opcional: Cierra el menú cuando hacés clic en cualquier link
+document.querySelectorAll('.header-button').forEach((link) => {
+  link.addEventListener('click', () => {
+    navLinks.classList.remove('active')
+  })
+})
